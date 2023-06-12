f59ca53049067b81a3e7978fb12f0440797673c0
rupeshbhondave1681 <bhondave.rupesh.1681@gmail.com>
2023-06-12 21:23:27 +0530

    Add Schema for find all suppliers who provide product in seafood Category.

---
+++ Query_2.sql
@@ -0,0 +1,15 @@
+/*Query2:Find all suppliers who provide products in the ‘Seafood’ category
+
+  Schema Description:In this query, I have select the supplier_id and contact_name as a Suppliers_name columns from the Suppliers table.
+         I have use a subquery to retrieve the supplier_id values from the categories table for products in the 'Seafood' category. 
+         The outer query then filters the suppliers based on those supplier_id values using the IN operator
+*/
+
+SELECT supplier_id,contact_name AS Suppliers_Name
+FROM suppliers 
+WHERE supplier_id IN
+ (
+  SELECT supplier_id
+  FROM categories
+  WHERE category_name = 'Seafood'
+);
