36b6523e86c0a2f651bff0e95cf0258a55873f3f
rupeshbhondave1681 <bhondave.rupesh.1681@gmail.com>
2023-06-12 21:32:10 +0530

    Add Schema for get total quantity of each sold product.

---
+++ Query_3.sql
@@ -0,0 +1,9 @@
+/*Query2:Get the total quantity of each product sold.
+
+  Schema Description: In this query, I have select the product_id column from the OrderDetails table.
+          use the SUM() function to calculate the total quantity sold by summing the quantity column.
+          then i have use the GROUP BY clause to group the results by the product_id.
+*/
+SELECT product_id, SUM(quantity) AS total_quantity_sold
+FROM Order_details
+GROUP BY product_id;
