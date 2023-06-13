b9e75297e23b9f3a73522987efa86520596a9d4d
rupeshbhondave1681 <bhondave.rupesh.1681@gmail.com>
2023-06-13 13:25:12 +0530

    Add a query for get total no of order placed by each Customer

--- Query_1.sql
+++ Query_1.sql
@@ -1,6 +1,6 @@
 /*Query1:Get the total number of orders placed by each customer
 
-  Schema Description:In this query, I have select the customer_id column from the Orders table 
+  Query Description:In this query, I have select the customer_id column from the Orders table 
   and use the COUNT(*) function to calculate the total number of orders for each customer.
   then use the GROUP BY clause to group the results by the customer_id.
 */
