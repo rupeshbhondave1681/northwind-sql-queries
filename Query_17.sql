c9f1088fe4a1e0eef4b61be171d975fc5a20c93d
rupeshbhondave1681 <bhondave.rupesh.1681@gmail.com>
2023-06-13 14:03:24 +0530

    Add SQL query to list distinct countries for shipped orders.

---
+++ Query_17.sql
@@ -0,0 +1,8 @@
+/*
+Query17:List all the distinct countries to which orders have been shipped.
+Query Description:In this query, the DISTINCT keyword is used to retrieve only the unique 
+country values from the "Customers" table.
+*/
+
+SELECT DISTINCT country
+FROM customers
